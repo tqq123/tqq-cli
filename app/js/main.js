e00da523afff0427687b22ac09c6db711a842b75
tqq <2521000359@qq.com>
2018-04-28 14:55:02 +0800

    add fastclick

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import fastclick from 'fastclick';
+
+fastclick.attach(document.body);
 
 Vue.config.productionTip = false;
 
